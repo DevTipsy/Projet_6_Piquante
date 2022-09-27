01e41fa8cbbb44765da70e8a37e7b49dcfeaea35
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2022-09-27 17:41:54 +0200

    Adding backend

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Tipsy:a7p2ap6E9EmCdh6M@cluster0.f5oate4.mongodb.net/test',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
