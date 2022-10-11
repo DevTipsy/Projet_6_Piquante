06f27efc63f33e142f7126cc5e4a87a76ab108a1
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2022-10-11 19:36:11 +0200

    update 4
    Fix ajout image+ajout dossier images

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -3,11 +3,8 @@ const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
-  delete sauceObject._userId;
   const sauce = new Sauce({
       ...sauceObject,
-      userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
       likes: 0,
       dislikes: 0,
