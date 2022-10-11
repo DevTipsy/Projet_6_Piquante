2972ebe6d9441deaaf6879f543fbd4fa83a1e018
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2022-10-11 20:00:20 +0200

    update 5
    Suppression de produits

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -66,23 +66,23 @@ exports.modifySauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id})
+  const userId = req.body.userId;
+  Sauce.findOne({_id: req.params.id})
       .then(sauce => {
-          if (sauce.userId != req.auth.userId) {
-              res.status(401).json({message: 'Not authorized'});
-          } else {
-              const filename = sauce.imageUrl.split('/images/')[1];
-              fs.unlink(`images/${filename}`, () => {
-                  Sauce.deleteOne({_id: req.params.id})
-                      .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                      .catch(error => res.status(401).json({ error }));
-              });
-          }
-      })
-      .catch( error => {
-          res.status(500).json({ error });
-      });
-};
+        console.log(req.body.userId);
+        if (userId !== process.userId)  {
+          return res.status(403).json({message : 'Non autorisé'});
+        } else{
+          const filename = sauce.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+              Sauce.deleteOne({_id: req.params.id})
+                  .then(res.status(200).json({message: 'Objet supprimé !'}))
+                  .catch(error => res.status(400).json({error}));
+          })
+      }
+    })
+      .catch(error => res.status(500).json({error}))
+};  
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find().then(
